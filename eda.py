7b45c28c7ed001df24258e8b87fe7d338ebb7e30
Theppitak <tata.theppitak@gmail.com>
2026-09-07 22:12:55 +0700

    Update eda.py

--- eda.py
+++ eda.py
@@ -4,20 +4,17 @@ import numpy as np
 
 df = pd.read_csv("Data/train.csv")
 
-# ────────────────────────────────
-# ขั้น 1: ทำความเข้าใจ data ก่อน
-# ────────────────────────────────
-# 1.1 ขนาด data
+
 print("Shape of data")
 print(f"Rows: {df.shape[0]}, Columns: {df.shape[1]}")
 
-# 1.2 ค่าหายมีที่ column ไหนบ้าง?
+
 print("\nMissing values in each column:")
 missing = df.isnull().sum()
 missing = missing[missing > 0].sort_values(ascending=False)
 print(missing.head(20))
 
-#ดูราคาบ้าน กราฟ
+
 print("\nPrice distribution:")
 print(df["SalePrice"].describe())
 plt.figure(figsize=(10, 6))
@@ -37,7 +34,7 @@ def clean_data(df):
     for col in none_cols:
         df[col] = df[col].fillna("None")
 
-# 2. ตัวเลขที่หายเพราะ "ไม่มี" จริงๆ
+
     zero_cols = ['GarageYrBlt', 'GarageArea', 'GarageCars',
                  'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF',
                  'TotalBsmtSF', 'MasVnrArea']
@@ -48,7 +45,7 @@ def clean_data(df):
     df['LotFrontage'] = df.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
     return df
 df_clean = clean_data(df)
-# columns พวกนี้มีลำดับชัดเจน Poor → Excellent
+
 quality_map = {"None": 0, "Po": 1, "Fa": 2, "TA": 3, "Gd": 4, "Ex": 5}
 quality_cols = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond',
                 'HeatingQC', 'KitchenQual', 'FireplaceQu',
@@ -56,7 +53,7 @@ quality_cols = ['ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond',
 
 for col in quality_cols:
     df_clean[col] = df_clean[col].map(quality_map)
-# columns ที่เหลือที่เป็น text ไม่มีลำดับ
+
 onehot_cols = ['MSZoning', 'Street', 'Alley', 'LotShape',
                'Neighborhood', 'BldgType', 'HouseStyle',
                'SaleType', 'SaleCondition', 'Foundation',
@@ -81,7 +78,7 @@ plt.show()
 print(f"Features ที่ดี: {good_features}")
 print(f"จำนวน: {len(good_features)} features")
 print("\n=== Missing After Cleaning ===")
-print(df_clean.isnull().sum().sum())  # ควรได้ 0
+print(df_clean.isnull().sum().sum()) 
 print(f"Before encoding: {df_clean.shape}")
 print(f"After encoding:  {df_encoded.shape}")
 print(df_clean.dtypes)
